0ddc7a47856c06fb68e0299a6d4801b339e5b40e
Scott <scott.dn91@gmail.com>
2021-11-05 05:55:10 +0700

    feat(api): 🎸 progress tracker

--- services/cover/src/lib.rs
+++ services/cover/src/lib.rs
@@ -1,9 +1,12 @@
 use ic_kit::ic::caller;
 use ic_kit::macros::{query, update};
 
-use crate::common::types::{CallerId, ReqId};
+use crate::common::types::{CallerId, CanisterId, ReqId};
 use crate::service::cover_service;
-use crate::service::types::{Error, NewValidationRequest, ProviderInfo, ValidationRequest};
+use crate::service::types::{
+    Error, NewValidationRequest, ProviderInfo, UpdateOnGoingProgressStatus, ValidationRequest,
+    ValidationResponse,
+};
 
 mod common;
 mod service;
@@ -33,4 +36,25 @@ fn get_all_pending_request() -> Vec<&'static ValidationRequest> {
 #[update]
 fn consume_request(provider_info: ProviderInfo) -> Result<Vec<&'static ValidationRequest>, Error> {
     cover_service::consume_request(provider_info)
+}
+
+#[query]
+fn get_on_going_progress_by_request_id(request_id: ReqId) -> Option<&'static ValidationResponse> {
+    cover_service::get_on_going_progress_by_request_id(request_id)
+}
+
+#[query]
+fn get_on_going_progress_by_canister_id(
+    canister_id: CanisterId,
+) -> Vec<&'static ValidationResponse> {
+    cover_service::get_on_going_progress_by_canister_id(canister_id)
+}
+
+#[update]
+fn update_on_going_progress_status(
+    request_validation_id: ReqId,
+    canister_id: CanisterId,
+    status: UpdateOnGoingProgressStatus,
+) -> Result<(), Error> {
+    cover_service::update_on_going_progress_status(request_validation_id, canister_id, status)
 }
