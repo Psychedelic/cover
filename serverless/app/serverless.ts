a5d71001a95aef9a67d5160153947eabb8164110
vojtech <vojtech.studenka@gmail.com>
2021-11-09 14:23:00 +0100

    draft

---
+++ serverless/app/serverless.ts
@@ -0,0 +1,32 @@
+import type { AWS } from '@serverless/typescript';
+
+import publish from '@functions/publish';
+
+const serverlessConfiguration: AWS = {
+  service: 'cover',
+  frameworkVersion: '2',
+  custom: {
+    webpack: {
+      webpackConfig: './webpack.config.js',
+      includeModules: true,
+    },
+  },
+  plugins: ['serverless-webpack'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs14.x',
+    region: 'us-west-2',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+    },
+    lambdaHashingVersion: '20201221',
+  },
+  // import the function via paths
+  functions: { publish },
+};
+
+module.exports = serverlessConfiguration;
