605379480e19cbf2cdcedfb49e5075b83a157911
rbialek <robert@fleek.io>
2021-11-09 19:19:06 +0100

    Updated cover idls

--- serverless/poll-cover/src/idls/cover.ts
+++ serverless/poll-cover/src/idls/cover.ts
@@ -1,43 +1,84 @@
 export const idlFactory = ({ IDL }) => {
-  const BuildParams = IDL.Record({
+  const ProviderInfo = IDL.Record({});
+  const BuildSettings = IDL.Record({
     'git_ref' : IDL.Text,
     'git_tag' : IDL.Text,
   });
-  const NewValidationRequest = IDL.Record({
+  const Request = IDL.Record({
+    'request_id' : IDL.Nat64,
     'canister_id' : IDL.Principal,
-    'validator_id' : IDL.Opt(IDL.Principal),
-    'build_settings' : BuildParams,
+    'created_at' : IDL.Text,
+    'caller_id' : IDL.Principal,
+    'build_settings' : BuildSettings,
   });
   const Error = IDL.Record({
     'debug_log' : IDL.Opt(IDL.Text),
     'code' : IDL.Text,
     'message' : IDL.Text,
   });
-  const ProviderInfo = IDL.Record({});
-  const ValidationRequest = IDL.Record({
-    'request_id' : IDL.Nat64,
+  const CreateRequest = IDL.Record({
     'canister_id' : IDL.Principal,
-    'caller_id' : IDL.Principal,
-    'build_settings' : BuildParams,
+    'build_settings' : BuildSettings,
+  });
+  const ProgressStatus = IDL.Variant({
+    'Error' : IDL.Null,
+    'Init' : IDL.Null,
+    'Finished' : IDL.Null,
+    'InProgress' : IDL.Null,
+  });
+  const Progress = IDL.Record({
+    'request_id' : IDL.Nat64,
+    'status' : ProgressStatus,
+    'wasm_checksum' : IDL.Opt(IDL.Text),
+    'updated_at' : IDL.Opt(IDL.Text),
+    'source_snapshot_url' : IDL.Opt(IDL.Text),
+    'canister_id' : IDL.Principal,
+    'git_checksum' : IDL.Opt(IDL.Text),
+    'canister_checksum' : IDL.Opt(IDL.Text),
+    'build_log_url' : IDL.Opt(IDL.Text),
+    'percentage' : IDL.Opt(IDL.Float32),
+    'started_at' : IDL.Text,
+  });
+  const UpdateProgress = IDL.Record({
+    'request_id' : IDL.Nat64,
+    'status' : ProgressStatus,
+    'wasm_checksum' : IDL.Opt(IDL.Text),
+    'source_snapshot_url' : IDL.Opt(IDL.Text),
+    'canister_id' : IDL.Principal,
+    'git_checksum' : IDL.Opt(IDL.Text),
+    'canister_checksum' : IDL.Opt(IDL.Text),
+    'build_log_url' : IDL.Opt(IDL.Text),
+    'percentage' : IDL.Opt(IDL.Float32),
   });
   return IDL.Service({
-    'add_request' : IDL.Func(
-        [NewValidationRequest],
-        [IDL.Variant({ 'Ok' : IDL.Null, 'Err' : Error })],
-        [],
-    ),
     'consume_request' : IDL.Func(
         [ProviderInfo],
-        [IDL.Variant({ 'Ok' : IDL.Vec(ValidationRequest), 'Err' : Error })],
+        [IDL.Variant({ 'Ok' : IDL.Vec(Request), 'Err' : Error })],
         [],
-    ),
-    'get_all_pending_request' : IDL.Func([], [IDL.Vec(ValidationRequest)], []),
-    'get_pending_request_by_id' : IDL.Func(
+      ),
+    'create_request' : IDL.Func(
+        [CreateRequest],
+        [IDL.Variant({ 'Ok' : IDL.Null, 'Err' : Error })],
+        [],
+      ),
+    'get_all_progress' : IDL.Func([], [IDL.Vec(Progress)], []),
+    'get_all_request' : IDL.Func([], [IDL.Vec(Request)], []),
+    'get_progress_by_canister_id' : IDL.Func(
+        [IDL.Principal],
+        [IDL.Vec(Progress)],
+        [],
+      ),
+    'get_progress_by_request_id' : IDL.Func(
         [IDL.Nat64],
-        [IDL.Opt(ValidationRequest)],
+        [IDL.Opt(Progress)],
         [],
-    ),
-    'whoami' : IDL.Func([], [IDL.Principal], []),
+      ),
+    'get_request_by_id' : IDL.Func([IDL.Nat64], [IDL.Opt(Request)], []),
+    'update_progress' : IDL.Func(
+        [UpdateProgress],
+        [IDL.Variant({ 'Ok' : IDL.Null, 'Err' : Error })],
+        [],
+      ),
   });
 };
 export const init = ({ IDL }) => { return []; };
