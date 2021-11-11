12b8e2698cad5fcd4b7e9e1b77b7c6e40f2e8c52
Scott <scott.dn91@gmail.com>
2021-11-12 00:02:16 +0700

    chore: 🚬 make apis same order with candid

--- services/cover/src/lib.rs
+++ services/cover/src/lib.rs
@@ -12,11 +12,6 @@ mod service;
 
 // TODO: history api
 
-#[update]
-fn create_request(_create_request: CreateRequest) -> Result<(), Error> {
-    cover_service::create_request(_create_request)
-}
-
 #[query]
 fn get_request_by_id(request_id: ReqId) -> Option<&'static Request> {
     cover_service::get_request_by_id(request_id)
@@ -25,11 +20,6 @@ fn get_request_by_id(request_id: ReqId) -> Option<&'static Request> {
 #[query]
 fn get_all_requests() -> Vec<&'static Request> {
     cover_service::get_all_requests()
-}
-
-#[update]
-fn consume_requests(provider_info: ProviderInfo) -> Result<Vec<&'static Request>, Error> {
-    cover_service::consume_requests(provider_info)
 }
 
 #[query]
@@ -47,13 +37,6 @@ fn get_all_progresses() -> Vec<&'static Progress> {
     cover_service::get_all_progresses()
 }
 
-#[update]
-fn update_progress(
-    _update_progress: UpdateProgress, // TODO: cdk bug????? param can not be same with fn!!!!!
-) -> Result<(), Error> {
-    cover_service::update_progress(_update_progress)
-}
-
 #[query]
 fn get_verification_by_canister_id(canister_id: CanisterId) -> Option<&'static Verification> {
     cover_service::get_verification_by_canister_id(&canister_id)
@@ -65,6 +48,18 @@ fn get_all_verifications() -> Vec<&'static Verification> {
 }
 
 #[update]
+fn consume_requests(provider_info: ProviderInfo) -> Result<Vec<&'static Request>, Error> {
+    cover_service::consume_requests(provider_info)
+}
+
+#[update]
+fn update_progress(
+    _update_progress: UpdateProgress, // TODO: cdk bug????? param can not be same with fn!!!!!
+) -> Result<(), Error> {
+    cover_service::update_progress(_update_progress)
+}
+
+#[update]
 fn add_verification(_add_verification: AddVerification) -> Result<(), Error> {
     cover_service::add_verification(_add_verification)
 }
@@ -72,6 +67,11 @@ fn add_verification(_add_verification: AddVerification) -> Result<(), Error> {
 #[update]
 fn update_verification(_update_verification: UpdateVerification) -> Result<(), Error> {
     cover_service::update_verification(_update_verification)
+}
+
+#[update]
+fn create_request(_create_request: CreateRequest) -> Result<(), Error> {
+    cover_service::create_request(_create_request)
 }
 
 #[update]
