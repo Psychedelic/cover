9362c7a631e2e43655963dfefc1f8a7b4853cfbe
Scott <scott.dn91@gmail.com>
2021-11-15 19:31:30 +0700

    refactor(store): 💡 rename request - error store

--- services/cover/src/service/store/request.rs
+++ services/cover/src/service/store/request.rs
@@ -1,7 +1,7 @@
 use std::collections::{BTreeMap, VecDeque};
 
 use crate::common::types::{CallerId, ReqId};
-use crate::service::store::error::ErrorKind;
+use crate::service::store::error::ErrorKindStore;
 use crate::service::time_utils;
 use crate::service::types::{CreateRequest, ProviderInfo, Request};
 
@@ -125,10 +125,10 @@ impl RequestStore {
     pub fn consume_requests(
         &mut self,
         provider_info: ProviderInfo,
-    ) -> Result<Vec<&Request>, ErrorKind> {
+    ) -> Result<Vec<&Request>, ErrorKindStore> {
         let from = self.last_consumed_request_id;
         if from >= self.last_request_id {
-            return Err(ErrorKind::RequestNotFound);
+            return Err(ErrorKindStore::RequestNotFound);
         }
         self.last_consumed_request_id =
             self.next_consume_request_id(self.last_consumed_request_id, self.last_request_id);
@@ -379,7 +379,7 @@ mod test {
 
             // error consume when no request
             let result = store.consume_requests(test_data::fake_provider_info1());
-            assert_eq!(result, Err(ErrorKind::RequestNotFound));
+            assert_eq!(result, Err(ErrorKindStore::RequestNotFound));
 
             store.fake_store_with_offset(offset, len as usize);
 
