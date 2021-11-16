cece4ced4be8187d2c5f615eb15dc7c7ef3c9020
Scott <scott.dn91@gmail.com>
2021-11-17 02:44:32 +0700

    chore: 🚬 better naming

--- services/cover/src/service/guard.rs
+++ services/cover/src/service/guard.rs
@@ -20,7 +20,7 @@ pub fn is_valid_provider<T, F: FnOnce() -> Result<T, Error>>(
     f: F,
 ) -> Result<T, Error> {
     get_provider_store()
-        .is_provider_exists(provider_id)
+        .provider_exists(provider_id)
         .then(|| f())
         .unwrap_or_else(|| Err(ErrorKindService::InvalidProvider.into()))
 }
