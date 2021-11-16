24a6f60d824e11ed97cab461afe2cafc3a330022
Scott <scott.dn91@gmail.com>
2021-11-16 17:53:31 +0700

    feat(service): 🎸 cover upgrade

--- services/cover/src/service/mod.rs
+++ services/cover/src/service/mod.rs
@@ -1,4 +1,5 @@
 use ic_kit::ic::{get, get_mut};
+use ic_kit::ic::{stable_restore, stable_store, trap};
 
 use crate::service::store::progress::ProgressStore;
 use crate::service::store::provider::ProviderStore;
@@ -19,7 +20,7 @@ fn get_request_store_mut() -> &'static mut RequestStore {
 }
 
 #[inline]
-fn get_request_store_registry() -> &'static RequestStore {
+fn get_request_store() -> &'static RequestStore {
     get()
 }
 
@@ -51,4 +52,53 @@ fn get_provider_store_mut() -> &'static mut ProviderStore {
 #[inline]
 fn get_provider_store() -> &'static ProviderStore {
     get()
+}
+
+/// These steps are atomic: If canister_pre_upgrade or canister_post_upgrade trap, the upgrade has failed, and the canister is reverted to the previous state. Otherwise, the upgrade has succeeded, and the old instance is discarded.
+/// fyi: https://sdk.dfinity.org/docs/interface-spec/index.html#system-api
+
+type InternalStableStoreAsRef = (
+    &'static RequestStore,
+    &'static ProgressStore,
+    &'static VerificationStore,
+    &'static ProviderStore,
+);
+
+pub fn pre_upgrade() {
+    if let Err(e) = stable_store::<InternalStableStoreAsRef>((
+        get_request_store(),
+        get_progress_store(),
+        get_verification_store(),
+        get_provider_store(),
+    )) {
+        trap(&format!(
+            "An error occurred when saving to stable memory (pre_upgrade): {:?}",
+            e
+        ));
+    };
+}
+
+type InternalStableStore = (
+    RequestStore,
+    ProgressStore,
+    VerificationStore,
+    ProviderStore,
+);
+
+pub fn post_upgrade() {
+    stable_restore::<InternalStableStore>()
+        .map(
+            |(request_store, progress_store, verification_store, provider_store)| {
+                (*get_request_store_mut()) = request_store;
+                (*get_progress_store_mut()) = progress_store;
+                (*get_verification_store_mut()) = verification_store;
+                (*get_provider_store_mut()) = provider_store;
+            },
+        )
+        .unwrap_or_else(|e| {
+            trap(&format!(
+                "An error occurred when loading from stable memory (post_upgrade): {:?}",
+                e
+            ));
+        });
 }
