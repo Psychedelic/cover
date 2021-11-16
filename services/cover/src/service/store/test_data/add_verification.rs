4e17e844aa62294531869e5ac6914ea264a384e5
rbialek <robert@fleek.io>
2021-11-16 20:04:18 +0100

    fix cargo tests to use git_ref

--- services/cover/src/service/store/test_data/add_verification.rs
+++ services/cover/src/service/store/test_data/add_verification.rs
@@ -5,7 +5,7 @@ pub fn fake_add_verification1(canister_id: CanisterId) -> AddVerification {
     AddVerification {
         canister_id,
         git_checksum: "git_checksum1".into(),
-        canister_checksum: "canister_checksum1".into(),
+        git_ref: "git_ref1".into(),
         wasm_checksum: "wasm_checksum1".into(),
         build_log_url: "build_log_url1".into(),
         source_snapshot_url: "source_snapshot_url1".into(),
@@ -16,7 +16,7 @@ pub fn fake_add_verification2(canister_id: CanisterId) -> AddVerification {
     AddVerification {
         canister_id,
         git_checksum: "git_checksum2".into(),
-        canister_checksum: "canister_checksum2".into(),
+        git_ref: "git_ref2".into(),
         wasm_checksum: "wasm_checksum2".into(),
         build_log_url: "build_log_url2".into(),
         source_snapshot_url: "source_snapshot_url2".into(),
@@ -27,7 +27,7 @@ pub fn fake_add_verification3(canister_id: CanisterId) -> AddVerification {
     AddVerification {
         canister_id,
         git_checksum: "git_checksum3".into(),
-        canister_checksum: "canister_checksum3".into(),
+        git_ref: "git_ref3".into(),
         wasm_checksum: "wasm_checksum3".into(),
         build_log_url: "build_log_url3".into(),
         source_snapshot_url: "source_snapshot_url3".into(),
