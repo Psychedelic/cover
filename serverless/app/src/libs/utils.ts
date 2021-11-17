f0f9ef409ad37ac92d1081f607668070352a3dd5
vojtech <vojtech.studenka@gmail.com>
2021-11-17 11:05:57 +0100

    added ENVs, fixed node-fetch issue

--- serverless/app/src/libs/utils.ts
+++ serverless/app/src/libs/utils.ts
@@ -6,12 +6,14 @@ const network = process.env.DFX_NETWORK || 'local';
  * @return "rrkah-fqaaa-aaaaa-aaaaq-cai"
  */
 const getCoverCanisterId = () => {
-    let id = process.env.COVER_CANISTER_ID;
-    if (!id) {
-        const net = network === 'local' ? 'local' : 'ic';
-        const canisters = require(`../../../../.dfx/${net}/canister_ids.json`);
-        id = canisters.cover[net] // returns canister_id
-    }
+    const id = process.env.COVER_CANISTER_ID;
+
+    // if (!id) {
+    //     const net = network === 'local' ? 'local' : 'ic';
+    //     // @todo: please replace this, do not use require outside of project directory
+    //     // const canisters = require(`../../../../.dfx/${net}/canister_ids.json`);
+    //     // id = canisters.cover[net] // returns canister_id
+    // }
     if (process.env.DEBUG) {console.log('COVER_CANISTER_ID', id)};
     return id;
 }
