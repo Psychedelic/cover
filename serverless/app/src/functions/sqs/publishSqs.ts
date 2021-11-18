2757d0b5f36fbafc08a9106a84cab36fea4ece87
rbialek <robert@fleek.io>
2021-11-18 14:43:17 +0100

    Connected cover plugin with SQS

--- serverless/app/src/functions/sqs/publishSqs.ts
+++ serverless/app/src/functions/sqs/publishSqs.ts
@@ -1,59 +1,59 @@
 import 'source-map-support/register';
 
-import { v4 as uuid } from 'uuid';
-import { middyfy } from '@libs/lambda';
-import { CoverPayload } from '@libs/coverPayload';
-import { APIGatewayProxyHandler } from 'aws-lambda';
-import { formatJSONResponse } from '@libs/apiGateway';
+import {middyfy} from '@libs/lambda';
+import {CoverPayload} from '@libs/coverPayload';
+import {APIGatewayProxyHandler} from 'aws-lambda';
+import {formatJSONResponse} from '@libs/apiGateway';
 import {
-  SQSClient,
-  SendMessageCommand,
+    SQSClient,
+    SendMessageCommand,
 } from '@aws-sdk/client-sqs';
 
-const QueueUrl = process.env.QUEUE_URL;
-// const QueueUrl = `http://localhost:9324/queue/cover-queue-cover-${process.env.STAGE}`
-// const QueueUrl = 'https://sqs.us-west-2.amazonaws.com/768127979193/cover-queue-cover-dev';
 
-const sqsClient = new SQSClient({ region: process.env.AWS_REGION });
+let QueueUrl = process.env.QUEUE_URL;
+if (process.env.STAGE == 'local') {
+    QueueUrl = `http://localhost:9324/queue/cover-queue-cover-${process.env.STAGE}`
+}
+if (process.env.STAGE == 'dev') {
+  QueueUrl = 'https://sqs.us-west-2.amazonaws.com/768127979193/cover-queue-cover-dev';
+}
+
+const sqsClient = new SQSClient({region: process.env.AWS_REGION});
 
 export const publishSqs: APIGatewayProxyHandler = async (
-  event
+    event
 ): Promise<any> => {
-  if (!event.body) {
-    return formatJSONResponse({
-      statusCode: 500,
-      body: `Error publishSqs: no data!`,
-    });
-  }
+    if (!event.body) {
+        return formatJSONResponse({
+            statusCode: 500,
+            body: `Error publishSqs: no data!`,
+        });
+    }
 
-  const coverPayload: CoverPayload = event.body;
-  console.log('Received', { QueueUrl, coverPayload });
+    const coverPayload: CoverPayload = event.body;
 
-  const body = {
-        responseCode: 200,
-        message: '',
-        messageId: '',
-      };
-  try {
-    const command = new SendMessageCommand({
-      QueueUrl,
-      MessageBody: JSON.stringify(coverPayload),
-      // MessageGroupId: mockEvent.git_checksum,
-      // MessageDeduplicationId: uuid(),
-    });
+    const response = {
+        statusCode: 200,
+        body: ''
+    };
+    try {
+        console.log('Sending', {
+            // env: process.env,
+            QueueUrl, coverPayload});
+        const command = new SendMessageCommand({
+            QueueUrl,
+            MessageBody: JSON.stringify(coverPayload),
+            // MessageGroupId: mockEvent.git_checksum,
+            // MessageDeduplicationId: uuid(),
+        });
+        await sqsClient.send(command);
+    } catch (e) {
+        console.error('Exception on queue', e);
+        response.body = `Error on send queue: ${e}`;
+        response.statusCode = 500;
+    }
 
-    return sqsClient.send(command);
-  } catch (e) {
-    responseBody.responseCode = 500;
-    console.error('Exception on queue', e);
-  }
-
-  const response = {
-    statusCode: 200,
-    body;
-  };
-
-  return formatJSONResponse(response);
+    return formatJSONResponse(response);
 };
 
 export const main = middyfy(publishSqs);
