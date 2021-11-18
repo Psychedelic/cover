48ae9d0c3e59d86f24b1fb9ea17190e6bf203d1a
BotchM <botchm@hotmail.com>
2021-11-18 14:59:34 +0200

    error hanlder

--- serverless/app/src/functions/sqs/publishSqs.ts
+++ serverless/app/src/functions/sqs/publishSqs.ts
@@ -17,8 +17,8 @@ const LOCAL = true;
 //   ? `http://localhost:9324/queue/cover-queue-cover-${process.env.STAGE}`
 //   : (process.env.QUEUE_URL as string);
 
-const QueueUrl =
-  'https://sqs.us-west-2.amazonaws.com/768127979193/cover-queue-cover-dev';
+// const QueueUrl = process.env.QUEUE_URL;
+const QueueUrl = 'https://sqs.us-west-2.amazonaws.com/768127979193/cover-queue-cover-dev';
 
 const MAX_MESSAGES_PER_BATCH = 10;
 const sqsClient = new SQSClient({ region: process.env.AWS_REGION });
@@ -26,6 +26,10 @@ const sqsClient = new SQSClient({ region: process.env.AWS_REGION });
 export const publishSqs: APIGatewayProxyHandler = async (
   event
 ): Promise<any> => {
+  if (!QueueUrl) {
+    throw new Error('No queue URL present in environment')
+  }
+
   if (!event.body) {
     return formatJSONResponse({
       statusCode: 500,
@@ -34,35 +38,27 @@ export const publishSqs: APIGatewayProxyHandler = async (
   }
 
   const coverPayload: CoverPayload = event.body;
-  const responseBody = {
-    responseCode: 200,
-    message: '',
-    messageId: '',
-  };
+  console.log(coverPayload);
 
-  try {
-    const command = new SendMessageCommand({
-      QueueUrl,
-      MessageBody: JSON.stringify(coverPayload),
-      // MessageGroupId: mockEvent.git_checksum,
-      // MessageDeduplicationId: uuid(),
-    });
+  const promises = []
+  const command = new SendMessageCommand({
+    QueueUrl,
+    MessageBody: JSON.stringify(coverPayload),
+    // MessageGroupId: mockEvent.git_checksum,
+    // MessageDeduplicationId: uuid(),
+  });
 
-    return sqsClient.send(command);
-  } catch (e) {
-    responseBody.responseCode = 500;
-    console.error('Exception on queue', e);
+  promises.push(sqsClient.send(command));
+
+  const failed = (await Promise.allSettled(promises))
+    .flat()
+    .filter(({ status }) => status === 'rejected')
+
+  if (failed.length) {
+    throw new Error(`${failed.length} commands could not be sent ${JSON.stringify(failed)}`)
   }
 
-  const response = {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(responseBody),
-  };
-
-  return formatJSONResponse(response);
+  return formatJSONResponse({ status: 'OK' })
 };
 
 export const main = middyfy(publishSqs);
