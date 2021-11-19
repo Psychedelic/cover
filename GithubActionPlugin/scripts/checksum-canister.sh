d72508072da51cb24bb2542541ff68369566891d
rbialek <robert@fleek.io>
2021-11-19 21:57:09 +0100

    Canister status script improvement

--- GithubActionPlugin/scripts/checksum-canister.sh
+++ GithubActionPlugin/scripts/checksum-canister.sh
@@ -10,4 +10,4 @@ resp=$(dfx canister --no-wallet --network $NETWORK info $CANISTER_ID)
 # Returns
 # > Controller: r7inp-6aaaa-aaaaa-aaabq-cai
 # > Module hash: 0x83ad30e332119989e5f7cd67a69d176349f50a6c5d2591c9786930d6b57cdabe
-echo $resp | awk '/hash: (.+)$/{print $5}'
+echo $resp | sed  -e 's/Module/\n\rModule/g'| awk '/hash: (.+)$/{print $3}'
