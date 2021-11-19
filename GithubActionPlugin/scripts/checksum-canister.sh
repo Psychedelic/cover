46ddae33dbcab31f292e818f8e2bbd336030a094
rbialek <robert@fleek.io>
2021-11-19 21:50:34 +0100

    Use network param in checksum checker, default to ic

--- GithubActionPlugin/scripts/checksum-canister.sh
+++ GithubActionPlugin/scripts/checksum-canister.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 CANISTER_ID=$1
-resp=$(dfx canister --no-wallet --network ic info $CANISTER_ID)
+if [[ -z $2 ]]; then
+  NETWORK=ic
+else
+  NETWORK=$2
+fi
+
+resp=$(dfx canister --no-wallet --network $NETWORK info $CANISTER_ID)
 # Returns
 # > Controller: r7inp-6aaaa-aaaaa-aaabq-cai
 # > Module hash: 0x83ad30e332119989e5f7cd67a69d176349f50a6c5d2591c9786930d6b57cdabe
