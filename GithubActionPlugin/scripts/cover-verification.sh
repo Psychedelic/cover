d203950e9bd366dcd7188f5a9ec0fce8766baad6
rbialek <robert@fleek.io>
2021-11-22 23:47:38 +0100

    Extract canister Id correctly from dfx info

--- GithubActionPlugin/scripts/cover-verification.sh
+++ GithubActionPlugin/scripts/cover-verification.sh
@@ -12,7 +12,7 @@ echo "Fetching verifier of $CANISTER_ID on $NETWORK"
 wasm=$(echo $resp |sed  -e 's/[;{]/\n\r/g'|sed -e 's/"//g'|grep wasm|awk '/ = (.+)$/{print $4}')
 echo "Fetching canister checksum $CANISTER_ID on $NETWORK"
 resp=$(dfx canister --no-wallet --network $NETWORK info $CANISTER_ID)
-cani=$(echo $resp | awk '/hash: (.+)$/{print $5}')
+cani=$(echo $resp |sed  -e 's/Module/\n\r/g'| grep hash| awk '/hash: (.+)$/{print $3}')
 
 
 echo "Wasm checksum: $wasm"
