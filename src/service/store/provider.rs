a4c38195446203f4da55b6d2299d8b0590815fbf
Claire Tran <claire.tran23@gmail.com>
2021-12-18 05:27:27 +0700

    chore: update provider store unit tests

--- src/service/store/provider.rs
+++ src/service/store/provider.rs
@@ -78,136 +78,108 @@ impl ProviderStore {
 
 #[cfg(test)]
 mod test {
-    use crate::service::store::test_data;
+    use ic_kit::*;
+
+    use crate::service::store::test_data::*;
 
     use super::*;
 
-    fn update_provider_gen(seed: u8) -> UpdateProvider {
-        if seed % 3 == 0 {
-            test_data::fake_update_provider1(&test_data::fake_canister1())
-        } else if seed % 3 == 1 {
-            test_data::fake_update_provider2(&test_data::fake_canister2())
-        } else {
-            test_data::fake_update_provider3(&test_data::fake_canister3())
-        }
-    }
-
-    fn add_provider_gen(seed: u8) -> AddProvider {
-        if seed % 3 == 0 {
-            test_data::fake_add_provider1(&test_data::fake_canister1())
-        } else if seed % 3 == 1 {
-            test_data::fake_add_provider2(&test_data::fake_canister2())
-        } else {
-            test_data::fake_add_provider3(&test_data::fake_canister3())
-        }
-    }
-
-    fn init_test_data(len: u8) -> ProviderStore {
+    fn init_test_data() -> ProviderStore {
         let mut store = ProviderStore::default();
-        for i in 0..len {
-            let result = store.add_provider(&test_data::caller_gen(i), add_provider_gen(i));
-            assert_eq!(result, Ok(()));
-        }
+
+        store
+            .add_provider(
+                &mock_principals::bob(),
+                fake_add_provider1(&mock_principals::alice()),
+            )
+            .unwrap();
+
         store
     }
 
     #[test]
     fn add_provider_ok() {
-        let mut store = init_test_data(3);
-        assert_eq!(store.provider.len(), 3);
-        for i in 0..store.provider.len() {
-            let result =
-                store.add_provider(&test_data::caller_gen(i as u8), add_provider_gen(i as u8));
-            assert_eq!(result, Err(ErrorKindStore::ExistedProvider));
-        }
+        let mut store = init_test_data();
+
+        assert_eq!(store.get_all_providers().len(), 1);
+
+        assert_eq!(
+            store.add_provider(
+                &mock_principals::alice(),
+                fake_add_provider1(&mock_principals::john()),
+            ),
+            Ok(())
+        );
+
+        assert_eq!(
+            store.add_provider(
+                &mock_principals::bob(),
+                fake_add_provider1(&mock_principals::john())
+            ),
+            Err(ErrorKindStore::ExistedProvider)
+        );
+
+        assert_eq!(store.get_all_providers().len(), 2);
     }
 
     #[test]
     fn update_provider_ok() {
-        let mut store = ProviderStore::default();
-        for i in 0..3 {
-            let update_provider = update_provider_gen(i as u8);
-            let caller_id = test_data::caller_gen(i as u8);
-            let result = store.update_provider(&caller_id, update_provider);
-            assert_eq!(result, Err(ErrorKindStore::ProviderNotFound));
-        }
-        let mut store = init_test_data(3);
-        for i in 0..store.provider.len() {
-            let update_provider = update_provider_gen(i as u8);
-            let caller_id = test_data::caller_gen(i as u8);
-            let result = store.update_provider(&caller_id, update_provider);
-            assert_eq!(result, Ok(()));
-        }
-        for i in 0..store.provider.len() {
-            let update_provider = update_provider_gen(i as u8);
-            let provider = store.provider.get(&update_provider.id).unwrap();
-            let caller_id = test_data::caller_gen(i as u8);
-            let now = time_utils::now_to_str();
-            assert_eq!(provider.id, update_provider.id);
-            assert_eq!(provider.name, update_provider.name);
-            assert_eq!(provider.memo, update_provider.memo);
-            assert_eq!(provider.created_at, now);
-            assert_eq!(provider.created_by, caller_id);
-            assert_eq!(provider.updated_at, now);
-            assert_eq!(provider.updated_by, caller_id);
-        }
-    }
+        let mut store = init_test_data();
 
-    #[test]
-    fn delete_provider_ok() {
-        let mut store = ProviderStore::default();
-        for i in 0..3 {
-            let provider = add_provider_gen(i as u8);
-            let result = store.delete_provider(&provider.id);
-            assert_eq!(result, Err(ErrorKindStore::ProviderNotFound));
-        }
-        let mut store = init_test_data(3);
-        for i in 0..store.provider.len() {
-            let provider = add_provider_gen(i as u8);
-            let result = store.delete_provider(&provider.id);
-            assert_eq!(result, Ok(()));
-        }
-        assert_eq!(store.provider.len(), 0);
+        assert_eq!(store.get_all_providers().len(), 1);
+
+        assert_eq!(
+            store.update_provider(
+                &mock_principals::bob(),
+                fake_update_provider1(&mock_principals::alice())
+            ),
+            Ok(())
+        );
+
+        assert_eq!(
+            store.get_provider_by_id(&mock_principals::alice()),
+            Some(&fake_provider_use_update_model(
+                &mock_principals::bob(),
+                fake_update_provider1(&mock_principals::alice())
+            ))
+        );
+
+        assert_eq!(
+            store.update_provider(
+                &mock_principals::bob(),
+                fake_update_provider2(&mock_principals::john())
+            ),
+            Err(ErrorKindStore::ProviderNotFound)
+        );
     }
 
     #[test]
     fn get_provider_by_canister_id_ok() {
-        let store = init_test_data(3);
-        for i in 0..store.provider.len() {
-            let provider = add_provider_gen(i as u8);
-            let provider = store.get_provider_by_id(&provider.id).unwrap();
-            let caller_id = test_data::caller_gen(i as u8);
-            let now = time_utils::now_to_str();
-            assert_eq!(provider.id, provider.id);
-            assert_eq!(provider.name, provider.name);
-            assert_eq!(provider.memo, provider.memo);
-            assert_eq!(provider.created_at, now);
-            assert_eq!(provider.created_by, caller_id);
-            assert_eq!(provider.updated_at, now);
-            assert_eq!(provider.updated_by, caller_id);
-        }
+        let store = init_test_data();
+
+        assert_eq!(
+            store.get_provider_by_id(&mock_principals::alice()),
+            Some(&fake_provider_use_add_model(
+                &mock_principals::bob(),
+                fake_add_provider1(&mock_principals::alice()),
+            ))
+        );
+
+        assert_eq!(store.get_provider_by_id(&mock_principals::john()), None);
+
+        assert_eq!(store.get_all_providers().len(), 1);
     }
 
     #[test]
     fn get_all_provider_ok() {
-        let store = init_test_data(3);
-        let providers = store.get_all_providers();
-        for i in 0..providers.len() {
-            let provider = add_provider_gen(i as u8);
-            let caller_id = test_data::caller_gen(i as u8);
-            let now = time_utils::now_to_str();
-            assert_eq!(
-                providers.contains(&&Provider {
-                    id: provider.id,
-                    name: provider.name,
-                    memo: provider.memo,
-                    created_at: now.clone(),
-                    created_by: caller_id,
-                    updated_at: now,
-                    updated_by: caller_id,
-                }),
-                true
-            );
-        }
+        let store = init_test_data();
+
+        assert_eq!(
+            store.get_all_providers(),
+            vec![&fake_provider_use_add_model(
+                &mock_principals::bob(),
+                fake_add_provider1(&mock_principals::alice())
+            )]
+        );
     }
 }
