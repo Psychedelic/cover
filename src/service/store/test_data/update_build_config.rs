a1d3e88c38075855a0f9d90ede246a10e3843010
Claire Tran <claire.tran23@gmail.com>
2021-12-22 05:53:10 +0700

    test: pr check test

--- src/service/store/test_data/update_build_config.rs
+++ src/service/store/test_data/update_build_config.rs
@@ -18,7 +18,7 @@ pub fn fake_update_build_config2() -> UpdateBuildConfig {
         repo_url: "https://github.com/Updated2".into(),
         user_repo_token: "thisisupdatedtoken2".into(),
         commit_hash: "234555".into(),
-        rust_version: "1.2.3".into(),
+        rust_version: "1.2.5".into(),
         dfx_version: "0.8.4".into(),
         optimize_times: 2,
     }
