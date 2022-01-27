c08d8b49c012ebac057af095ae3edbc6ea27c627
Scott Dn <scott.dn91@gmail.com>
2022-01-28 04:31:59 +0700

    test counter

--- src/api/test.rs
+++ src/api/test.rs
@@ -1,16 +1,16 @@
 use ic_kit::candid::candid_method;
 use ic_kit::macros::{query, update};
 
-type COUNTER = u32;
+type Counter = u32;
 
 #[update(name = "test")]
 #[candid_method(update, rename = "test")]
 fn test() {
-    *(ic_kit::ic::get_mut::<COUNTER>()) += 1;
+    *(ic_kit::ic::get_mut::<Counter>()) += 1;
 }
 
 #[query(name = "check")]
 #[candid_method(query, rename = "check")]
 fn check() -> u32 {
-    *ic_kit::ic::get::<COUNTER>()
+    *ic_kit::ic::get::<Counter>()
 }
