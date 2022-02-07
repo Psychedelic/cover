b2724523128eb8d5bd823961de31a2f10bd48b94
Scott Dn <scott.dn91@gmail.com>
2022-02-08 02:39:51 +0700

    fix: getActivities should be public

--- src/api/activity.rs
+++ src/api/activity.rs
@@ -2,11 +2,10 @@ use ic_kit::candid::candid_method;
 use ic_kit::macros::query;
 
 use crate::service::activity;
-use crate::service::guard::is_builder;
 use crate::service::model::activity::Activity;
 use crate::service::model::pagination::{Pagination, PaginationInfo};
 
-#[query(name = "getActivities", guard = "is_builder")]
+#[query(name = "getActivities")]
 #[candid_method(query, rename = "getActivities")]
 fn get_activities(pagination_info: PaginationInfo) -> Pagination<&'static Activity> {
     activity::get_activities(&pagination_info)
