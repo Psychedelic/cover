52fd387bf60744bf5059760251f73b411c19842a
Scott Dn <scott.dn91@gmail.com>
2022-02-22 03:28:18 +0700

    fix: init candid

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,6 @@ mod common;
 mod service;
 mod upgrade;
 
-#[cfg(any(target_arch = "wasm32"))]
-fn main() {}
-
-#[cfg(not(any(target_arch = "wasm32", test)))]
-fn main() {
-    use crate::common::types::*;
-    use crate::service::model::activity::*;
-    use crate::service::model::build_config::*;
-    use crate::service::model::error::*;
-    use crate::service::model::pagination::*;
-    use crate::service::model::stats::*;
-    use crate::service::model::verification::*;
-
-    ic_kit::candid::export_service!();
-    std::print!("{}", __export_service());
-}
-
 // ========================================================================================================
 // INIT
 //   - Admin
@@ -59,4 +42,21 @@ fn init(config: Option<Config>) {
             builder.iter().for_each(builder::add_builder);
         }
     }
+}
+
+#[cfg(any(target_arch = "wasm32"))]
+fn main() {}
+
+#[cfg(not(any(target_arch = "wasm32", test)))]
+fn main() {
+    use crate::common::types::*;
+    use crate::service::model::activity::*;
+    use crate::service::model::build_config::*;
+    use crate::service::model::error::*;
+    use crate::service::model::pagination::*;
+    use crate::service::model::stats::*;
+    use crate::service::model::verification::*;
+
+    ic_kit::candid::export_service!();
+    std::print!("{}", __export_service());
 }
