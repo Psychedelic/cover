d65b485531e2c993c1a8270f3b916f2367f43792
Scott Dn <scott.dn91@gmail.com>
2022-09-22 01:30:55 +0700

    chore: refactor

--- src/model/build_config.rs
+++ src/model/build_config.rs
@@ -13,7 +13,7 @@ pub struct BuildConfig {
     pub rust_version: Option<String>,
     pub dfx_version: String,
     pub optimize_count: u8,
-    pub updated_at: u64,
+    pub updated_at: u64, // Current system time given as nanoseconds since 1970-01-01
 }
 
 #[derive(CandidType, Deserialize)]
